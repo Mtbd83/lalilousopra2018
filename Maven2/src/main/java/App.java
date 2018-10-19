58e6b5a6dd95214ae26c60b793bb05a99553c1e8
Mathieu <mathieu@MacBook-Air-de-Mathieu.local>
2018-10-19 12:09:34 +0200

    partie de mathieu

---
+++ Maven2/src/main/java/App.java
@@ -0,0 +1,10 @@
+
+public class App {
+
+	public static void main(String[] args) {
+		Class_C cc = new Class_C();
+		cc.afficher();
+		
+	}
+
+}
---
+++ Maven2/src/main/java/Class_C.java
@@ -0,0 +1,6 @@
+
+public class Class_C {
+void afficher() {
+	System.out.println("Ceci est la class C designed by Mathieu le Grand l'Unique");
+}
+}
