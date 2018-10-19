286866209d04a7198786a9da9d7502e1246ba71e
Nandrasco <andrieu15@hotmail.com>
2018-10-19 12:02:40 +0200

    Version de NIco de Projet MAven comprenant la classe D

---
+++ src/main/java/classes/ClassD.java
@@ -0,0 +1,22 @@
+package classes;
+
+public class ClassD {
+	private String str="Welcome on the world's finest tour";
+
+	public String getStr() {
+		return str;
+	}
+
+	public void setStr(String str) {
+		this.str = str;
+	}
+
+	public ClassD(String str) {
+		this.str =str;
+	}
+
+	public ClassD() {
+		this.str=this.str;	
+}
+	
+}
---
+++ src/main/java/util/App.java
@@ -0,0 +1,13 @@
+package util;
+
+import classes.ClassD;
+
+public class App {
+	public void main (String[] args) {
+	ClassD d = new ClassD("Eh bonsoiir");
+	ClassD dd = new ClassD();
+	System.out.println(d);
+	System.out.println("Coucou");
+	dd.getStr();
+}
+}
